22573869ebe22b958086553185d5f97721cb46a1
BrunoVilela <brunovilelasilva@hotmail.com>
2016-06-14 16:08:21 -0500

    Change style at simulation functions

--- SimulationFunctions.R
+++ SimulationFunctions.R
@@ -1,4 +1,4 @@
-####################################################################################
+#==================================================================
 # SimulationFunctions.R
 #
 # Contains utility functions for simulation of cultural evolution in space and time
@@ -10,17 +10,17 @@
 # 7 Jun 2016 
 # Carlos A. Botero
 # Washington University in Saint Louis
-####################################################################################
-
+#==================================================================
+# Load required packages
 library(gtools)
 library(ape)
 library(adephylo)
 library(diversitree)
 
-############################################
-# first some simple hexagonal grid functions
+#==================================================================
+# First some simple hexagonal grid functions
 
-# function to check whether a neighbor is out of bounds in the
+# Function to check whether a neighbor is out of bounds in the
 checkNeighbor <- function (myHex, direction, myWorld) {
   Neigh <- as.numeric(myHex + direction)
   if (length(which(myWorld$x == Neigh[1] & 
@@ -32,7 +32,8 @@ checkNeighbor <- function (myHex, direction, myWorld) {
   }
 }
 
-# returns the coordinates of all possible neighbors (NA if neighbor is outside of desired grid system)
+# Returns the coordinates of all possible neighbors 
+# (NA if neighbor is outside of desired grid system)
 neighbors <- function(myHex, check = TRUE, myWorld) {
   if (check) {
     myNeighbors <- as.data.frame(rbind(
@@ -56,21 +57,28 @@ neighbors <- function(myHex, check = TRUE, myWorld) {
   return(myNeighbors)
 }
 
-####################################
-# build an hexagonal grid with a radius of R cells over which to simulate 
+#==================================================================
+# Build an hexagonal grid with a radius of R cells over which to simulate 
 # cultural evolution. Assume a proportion P of the world is appropriate for foraging
 # (env = 1; trait = 1) and the rest is good for domestication (env = 2; trait = 2)
+
 BuildWorld <- function (R, P) {
   myWorld <- as.data.frame(matrix(c(0, 0, 0), 1, 3))
-  names(myWorld) <- c('x', 'y', 'z') # cube coordinates for hexagonal grid systems (see http://www.redblobgames.com/grids/hexagons/)
+  
+  # Cube coordinates for hexagonal grid systems 
+  # (see http://www.redblobgames.com/grids/hexagons/)
+  names(myWorld) <- c('x', 'y', 'z') 
+  
   myWorld$Parent <- NA
   myWorld$BirthT <- NA
   myWorld$Trait <- NA
+  
   if (runif(1) <= P) {
     myWorld$Environment <- 2
   } else {
     myWorld$Environment <- 1
   }
+  
   for (i in 1:R) {
     for (j in 1:nrow(myWorld)) {
       myHex <- myWorld[j, c('x', 'y', 'z')]
@@ -84,7 +92,9 @@ BuildWorld <- function (R, P) {
           } else { 
             ThisEnv <- 1
           } 
-          myWorld <- rbind(myWorld, as.numeric(c(myneighbors[k, ], NA, NA, NA, ThisEnv)))
+          myWorld <- rbind(myWorld, 
+                           as.numeric(c(myneighbors[k, ],
+                                        NA, NA, NA, ThisEnv)))
         }
       }
     }
@@ -94,14 +104,14 @@ BuildWorld <- function (R, P) {
   return(myWorld)
 }
 
-####################################################
+#==================================================================
 getTargets <- function(myHex, myWorld, takeover) {
   
   AllTargets <- na.omit(neighbors(myHex, check = TRUE,
                                   myWorld = myWorld))
   PosTargets <- NULL
   
-  # figure out which of the neighboring cells are good options for this context
+  # Figure out which of the neighboring cells are good options for this context
   nAll <- nrow(AllTargets)
   if (!takeover) {
     for (j in 1:nAll) { # empty cells for colonization
@@ -127,8 +137,9 @@ getTargets <- function(myHex, myWorld, takeover) {
   return(PosTargets)
 }
 
-#########
-Speciate <- function(myT, Parent, PosTargets, myWorld, mytree, NodeData, takeover) {
+#==================================================================
+Speciate <- function(myT, Parent, PosTargets, myWorld, 
+                     mytree, NodeData, takeover) {
   # create descendant lineage
   if (length(PosTargets) > 1) {
     NewSoc <- sample(PosTargets, 1)
@@ -138,17 +149,23 @@ Speciate <- function(myT, Parent, PosTargets, myWorld, mytree, NodeData, takeove
   
   if (is.null(mytree)) { 
     # create a phylo object
-    mytree <- read.tree(text = paste0("(t", Parent, ":", myT, ",t", NewSoc, ":", myT, ");")) 
+    mytree <- read.tree(text = paste0("(t", Parent, ":",
+                                      myT, ",t", NewSoc,
+                                      ":", myT, ");")) 
     NodeData[1, ] <- c(1, Parent)
     NodeData[2, ] <- c(2, NewSoc)
   } else {
     # add a bifurcation to the node that used to be the parent
     BL <- myT - distRoot(mytree, 1, method = 'patristic')
-    newtips <- read.tree(text = paste0("(t", Parent, ":", BL, ",t",NewSoc, ":", BL, ");")) 
+    newtips <- read.tree(text = paste0("(t", Parent, ":",
+                                       BL, ",t", NewSoc,
+                                       ":", BL, ");")) 
     
     OldParentalNode <- NodeData$Node[which(NodeData$Tip == Parent)]
     
-    mytree <- read.tree(text = write.tree(bind.tree(mytree, newtips, where = OldParentalNode),
+    mytree <- read.tree(text = write.tree(bind.tree(mytree, 
+                                                    newtips,
+                                                    where = OldParentalNode),
                                           file = ''))
     
     # update NodeData
@@ -189,22 +206,25 @@ Speciate <- function(myT, Parent, PosTargets, myWorld, mytree, NodeData, takeove
         } # foraging evolves
       }
     }
-  } else { # This is a Take Over event (the earlier soc has already been wiped out of the phylogeny
-    # outside of this function and now all is left is to replace it by a descendant of the parent)
+  } else { # This is a Take Over event 
+    # (the earlier soc has already been wiped out of the phylogeny
+    # outside of this function and now all is left is to replace it
+    # by a descendant of the parent)
     myWorld$Trait[NewSoc] <- myWorld$Trait[Parent] 
   } 
   
-  return(list("myWorld" = myWorld, "mytree" = mytree, "NodeData" = NodeData))
+  return(list("myWorld" = myWorld, "mytree" = mytree,
+              "NodeData" = NodeData))
 }
 
-########
+#==================================================================
 Extinct <- function(mytree, NodeData, myWorld, Ext.tip) {
   
   # remove the from phylogeny
-  mytree <- drop.tip(mytree, tip = NodeData$Node[which(NodeData$Tip == Ext.tip)])
+  mytree <- drop.tip(mytree, tip = NodeData$Node[NodeData$Tip == Ext.tip])
   
   # update NodeData
-  NodeData <- as.data.frame(matrix(NA,length(mytree$tip.label), 2))
+  NodeData <- as.data.frame(matrix(NA, length(mytree$tip.label), 2))
   names(NodeData) <- c('Node', 'Tip')
   NodeData[, 1] <- 1:length(mytree$tip.label) 
   NodeData[, 2] <- as.numeric(gsub('t', '', mytree$tip.label))
@@ -212,15 +232,16 @@ Extinct <- function(mytree, NodeData, myWorld, Ext.tip) {
   # remove tip from map
   myWorld$Parent[Ext.tip] <- myWorld$BirthT[Ext.tip] <- myWorld$Trait[Ext.tip] <- NA
   
-  return(list("myWorld" = myWorld, "mytree" = mytree, "NodeData" = NodeData))
+  return(list("myWorld" = myWorld, "mytree" = mytree,
+              "NodeData" = NodeData))
 }
 
-##########################################
+#==================================================================
 RunSim <- function(myWorld, P.extinction, P.speciation, 
                    P.diffusion, P.Arisal, P.TakeOver) {
   myT <- 0
   
-  # initialize parameters we will use later to build the phylogeny
+  # Initialize parameters we will use later to build the phylogeny
   rootnode <- nrow(myWorld) + 1 # standard convention for root node number
   
   # set the seed for simulation
@@ -229,7 +250,7 @@ RunSim <- function(myWorld, P.extinction, P.speciation,
   myWorld$BirthT[start] <- 0 # starts at time zero
   myWorld$Trait[start] <- 1 # assumes ancestral state is forager
   
-  # keep track of the tip numbers for each position in myWorld (when colonized)
+  # Keep track of the tip numbers for each position in myWorld (when colonized)
   NodeData <- as.data.frame(matrix(c(rootnode, start), 1, 2))
   names(NodeData) <- c('Node', 'Tip') 
   
@@ -284,55 +305,75 @@ RunSim <- function(myWorld, P.extinction, P.speciation,
         for (i in UsedCells) { 
           myHex <- myWorld[i, c('x', 'y', 'z')]
           PosTargets <- getTargets(myHex, myWorld, takeover = T)
-          PosTargets <- PosTargets[which(myWorld$Trait[PosTargets] != myWorld$Trait[i])]
+          PosTargets <- PosTargets[myWorld$Trait[PosTargets] != myWorld$Trait[i]]
           
-          if (length(PosTargets) > 1) { NewSoc <- sample(PosTargets, 1) } 
-          else { NewSoc <- PosTargets }
+          if (length(PosTargets) > 1) {
+            NewSoc <- sample(PosTargets, 1)
+          } else { 
+            NewSoc <- PosTargets
+          }
           
           if (length(NewSoc) == 1) {
             if (myWorld$Trait[i] == 1){
               if (myWorld$Environment[NewSoc] == 1) {
-                if (runif(1) < P.diffusion[1,1]) { myWorld$Trait[NewSoc] <- myWorld$Trait[i] }
+                if (runif(1) < P.diffusion[1, 1]) {
+                  myWorld$Trait[NewSoc] <- myWorld$Trait[i]
+                }
+              } else {
+                if (runif(1) < P.diffusion[1, 2]) {
+                  myWorld$Trait[NewSoc] <- myWorld$Trait[i]
+                }
               }
-              else {
-                if (runif(1) < P.diffusion[1,2]) { myWorld$Trait[NewSoc] <- myWorld$Trait[i] }
-              }
-            }
-            else {
+            } else {
               if (myWorld$Environment[NewSoc] == 1) {
-                if (runif(1) < P.diffusion[2,1]) { myWorld$Trait[NewSoc] <- myWorld$Trait[i] }
-              }
-              else {
-                if (runif(1) < P.diffusion[2,2]) { myWorld$Trait[NewSoc] <- myWorld$Trait[i] }
+                if (runif(1) < P.diffusion[2, 1]) { 
+                  myWorld$Trait[NewSoc] <- myWorld$Trait[i]
+                }
+              } else {
+                if (runif(1) < P.diffusion[2,2]) {
+                  myWorld$Trait[NewSoc] <- myWorld$Trait[i]
+                }
               }
             }
           }
         }
       }
       
-      # allow possibility of hostile take overs (phylogenies DO change)
+      # Allow possibility of hostile take overs (phylogenies DO change)
       if (length(UsedCells) > 1) {
         for (i in UsedCells) {   
           myHex <- myWorld[i, c('x', 'y', 'z')]
-          PosTargets <- getTargets(myHex, myWorld, takeover = T)
-          PosTargets <- PosTargets[which(myWorld$Trait[PosTargets] != myWorld$Trait[i])]
+          PosTargets <- getTargets(myHex, myWorld, takeover = TRUE)
+          PosTargets <- PosTargets[myWorld$Trait[PosTargets] != myWorld$Trait[i]]
           
-          if (length(PosTargets) > 1) { Ext.tip <- sample(PosTargets, 1) } 
-          else { Ext.tip <- PosTargets }
+          if (length(PosTargets) > 1) { 
+            Ext.tip <- sample(PosTargets, 1)
+          } else {
+            Ext.tip <- PosTargets 
+          }
           
           if (length(Ext.tip) == 1) { 
-            TakeOver <- F # baseline
+            TakeOver <- FALSE # baseline
             if (myWorld$Trait[i] == myWorld$Environment[i]) { # Source is in right habitat
               if (myWorld$Trait[i] == myWorld$Environment[Ext.tip]) { # target is also in appropriate habitat
-                if (runif(1) < P.TakeOver[1,1]) { TakeOver <- T }
+                if (runif(1) < P.TakeOver[1,1]) {
+                  TakeOver <- TRUE 
+                }
+              } else {
+                if (runif(1) < P.TakeOver[2, 1]) { 
+                  TakeOver <- TRUE 
+                }
               }
-              else { if (runif(1) < P.TakeOver[2,1]) { TakeOver <- T } }
-            }
-            else {
+            } else {
               if (myWorld$Trait[i] == myWorld$Environment[Ext.tip]) { # target is also in appropriate habitat
-                if (runif(1) < P.TakeOver[1,2]) { TakeOver <- T }
+                if (runif(1) < P.TakeOver[1, 2]) {
+                  TakeOver <- T 
+                }
+              } else {
+                if (runif(1) < P.TakeOver[2, 2]) { 
+                  TakeOver <- T 
+                }
               }
-              else { if (runif(1) < P.TakeOver[2,2]) { TakeOver <- T } }
             }
             
             if (TakeOver) {
@@ -344,7 +385,9 @@ RunSim <- function(myWorld, P.extinction, P.speciation,
               Temp <- Speciate(myT = myT, Parent = i, PosTargets = Ext.tip, 
                                myWorld = myWorld, mytree = mytree, NodeData = NodeData, 
                                takeover = T)
-              myWorld <- Temp$myWorld; mytree <- Temp$mytree; NodeData <- Temp$NodeData
+              myWorld <- Temp$myWorld
+              mytree <- Temp$mytree
+              NodeData <- Temp$NodeData
             }
           }
         }
@@ -358,29 +401,36 @@ RunSim <- function(myWorld, P.extinction, P.speciation,
     if (sum.notna > 1) { 
       myOrder <- sample(who.notna,
                         sum.notna)
-    } else {myOrder <- who.notna}
+    } else {
+      myOrder <- who.notna
+    }
     
     # go one by one allowing each society a chance to reproduce
-    for (i in 1:sum.notna){
+    for (i in 1:sum.notna) {
       Row.In.Node.Data <- which(NodeData$Tip == myOrder[i])
       
       if (myWorld$Trait[myOrder[i]] == 1) {
-        if (myWorld$Environment[myOrder[i]] == 1) { Pspec <- P.speciation["For","EnvF"] }
-        else { Pspec <- P.speciation["For","EnvD"] }
-      }
-      else {
-        if (myWorld$Environment[myOrder[i]] == 1) { Pspec <- P.speciation["Dom","EnvF"] }
-        else { Pspec <- P.speciation["Dom","EnvD"] }
+        if (myWorld$Environment[myOrder[i]] == 1) { 
+          Pspec <- P.speciation["For","EnvF"]
+        } else {
+          Pspec <- P.speciation["For","EnvD"] 
+        }
+      } else {
+        if (myWorld$Environment[myOrder[i]] == 1) {
+          Pspec <- P.speciation["Dom","EnvF"]
+        } else {
+          Pspec <- P.speciation["Dom","EnvD"]
+        }
       }
       
       if (runif(1) <= Pspec) {
         # speciate (i.e., send diaspora to an adjacent empty cell)
         myHex <- myWorld[myOrder[i], c('x', 'y', 'z')]
-        PosTargets <- getTargets(myHex, myWorld, takeover = F)
+        PosTargets <- getTargets(myHex, myWorld, takeover = FALSE)
         if (!is.null(PosTargets)) {
           Temp <- Speciate(myT = myT, Parent = myOrder[i], PosTargets = PosTargets, 
                            myWorld = myWorld, mytree = mytree, NodeData = NodeData, 
-                           takeover = F)
+                           takeover = FALSE)
           
           myWorld <- Temp$myWorld
           mytree <- Temp$mytree
@@ -389,12 +439,14 @@ RunSim <- function(myWorld, P.extinction, P.speciation,
       }
     }
     
-    if(!is.null(mytree)) {
+    if (!is.null(mytree)) {
       # extend the tips of branches that did not reproduce to maintain an ultrametric tree
       for (i in 1:length(mytree$tip.label)) {
         if (distRoot(mytree, tips = i, method="patristic") < myT) {
-          ThisBranch <- which(mytree$edge[,2]==i)
-          mytree$edge.length[ThisBranch] <- mytree$edge.length[ThisBranch] + (myT-distRoot(mytree, tips = i, method="patristic"))
+          ThisBranch <- which(mytree$edge[, 2] == i)
+          dist.root <- distRoot(mytree, tips = i, method = "patristic")
+          sub <- (myT - dist.root)
+          mytree$edge.length[ThisBranch] <- mytree$edge.length[ThisBranch] + sub
         }
       }
     }
@@ -403,15 +455,20 @@ RunSim <- function(myWorld, P.extinction, P.speciation,
   return (list('mytree' = mytree, 'NodeData' = NodeData, 'myWorld' = myWorld))
 }
 
+
+#==================================================================
 # Plot output (single tree)
 myplot <- function(RunSim.Output, i) {
-  par(oma=c(1,1,3,1))
+  par(oma = c(1, 1, 3, 1))
   mytree <- RunSim.Output[[1]][[i]]
   myWorld <- RunSim.Output[[2]][[1]]
   row.names(myWorld) <- myWorld$TipLabel
-  myWorld <- myWorld[,c("Trait","Environment")]
-  trait.plot(mytree, dat = myWorld, cols = list("Trait"=c('blue','red'),"Environment"=c('Black','Brown')),
-             lab = list("Trait"=c('Forager', 'Domesticator'), "Environment"=c('Good4For', 'Good4Dom')),
-             type='p', w = 1/70)
-  title(paste(deparse(substitute(RunSim.Output)), i), outer=T)
+  myWorld <- myWorld[, c("Trait", "Environment")]
+  colors <- list("Trait" = c('blue', 'red'), 
+                 "Environment" = c('Black','Brown'))
+  labels <- list("Trait" = c('Forager', 'Domesticator'),
+                 "Environment" = c('Good4For', 'Good4Dom'))
+  trait.plot(mytree, dat = myWorld, cols = colors,
+             lab = labels, type = 'p', w = 1/70)
+  title(paste(deparse(substitute(RunSim.Output)), i), outer = TRUE)
 }
