071ba70277fb5922aacb7074fd89a9289efdd88f
Ty Tuff <tuff@wustl.edu>
2016-06-21 16:52:57 -0500

    change save file name for more indexing

---
+++ run_simulation_cluster (Autosaved).R
@@ -0,0 +1,97 @@
+# Run the full model in a cluster. This version writes files to a cluster output folder.
+rm(list = ls())
+source("Functions/Build_world_function.R")
+source("Functions/Auxiliary_functions.R")
+myWorld <- BuildWorld(R = 3, P = 0.8)
+source('Functions/Possible_combinations_of_movement_function.R')
+
+sim_run_cluster <- function(replicate_cycle, combo_number, myWorld) {
+  
+  chosen_combo <- combo_of_choice(combo_number, FALSE)
+  
+  if (chosen_combo[[2]] == "Speciate") {
+    P.speciation <- parameters(0.1, 0.1, 0.1, 0.1, "For", "Dom", "For", "Dom")
+  } else {
+    P.speciation <- parameters(0, 0, 0, 0, "For", "Dom", "For", "Dom")
+  }
+  
+  if (chosen_combo[[2]] == "Extinct") {
+#<<<<<<< HEAD
+    P.extinction  <- parameters(0, 0, 0.01, 0.01, "For", "Dom", "For", "Dom")
+#=======
+    P.extinction  <- parameters(0.01, 0.1, 0.1, 0.01, "For", "Dom", "For", "Dom")
+#>>>>>>> 0fcc7b64bc6b1c00b254ddbbdbae02201075966f
+  } else {
+    P.extinction  <- parameters(0, 0, 0, 0, "For", "Dom", "For", "Dom")
+  }
+  
+  if (chosen_combo[[2]] == "Diffusion") {
+    P.diffusion <- parameters(0.1, 0.1, 0.1, 0.1, "For", "Dom", "For", "Dom") 
+  } else {
+    P.diffusion <- parameters(0, 0, 0, 0, "For", "Dom", "For", "Dom")
+  }
+  
+  if (chosen_combo[[2]] == "Takeover") {
+    P.TakeOver <- parameters(0.1, 0.1, 0.1, 0.1, "For", "Dom", "For", "Dom")
+  } else {P.TakeOver <- parameters(0, 0, 0, 0, "For", "Dom", "For", "Dom")
+  }
+  
+  if (chosen_combo[[2]] == "Random_new_origin") {
+    P.Arisal <- parameters(0.1, 0.1, 0.1, 0.1, "For", "Dom", "For", "Dom") 
+  } else {
+    P.Arisal <- parameters(0, 0, 0, 0, "For", "Dom", "For", "Dom")
+  }
+  
+  myOut <- RunSimUltimate(myWorld, P.extinction, P.speciation, 
+                          P.diffusion, P.Arisal, P.TakeOver,
+                          N.steps = 50)
+  
+  save(myOut, file = paste0("cluster outputs/myOut_replicate_", formatC(replicate_cycle, width = 2,flag = 0), 
+                            "_function_combination_type_", formatC(combo_number, width = 2,flag = 0), "_",
+                            "parameters_", c(P.Arisal),
+                            as.integer(Sys.time()), " Results.Rdata"))
+}
+
+
+a <- Sys.time()
+library(parallel)
+
+
+# Set up cluster
+cl <- makeCluster(detectCores() , type = "PSOCK")
+
+# Push resources out to cluster
+clusterEvalQ(cl, library(gtools))
+clusterEvalQ(cl, library(ape))
+clusterEvalQ(cl, library(adephylo))
+clusterEvalQ(cl, library(diversitree))
+clusterEvalQ(cl, source("Functions/Arisal_module.R"))
+clusterEvalQ(cl, source("Functions/Auxiliary_functions.R"))
+clusterEvalQ(cl, source("Functions/Build_world_function.R"))
+clusterEvalQ(cl, source("Functions/Complete_Model.R"))
+clusterEvalQ(cl, source("Functions/Diffusion_module.R"))
+clusterEvalQ(cl, source("Functions/Extinction_module.R"))
+clusterEvalQ(cl, source("Functions/Speciate_function.R"))
+clusterEvalQ(cl, source("Functions/Speciation_function.R"))
+clusterEvalQ(cl, source("Functions/Takeover_function.R"))
+clusterEvalQ(cl, source("Functions/SpeciationTakeover_Module.R"))
+clusterEvalQ(cl, source("Functions/Possible_combinations_of_movement_function.R"))
+clusterEvalQ(cl, source("Functions/Ultimate_run_simulations.R"))
+
+
+# lset are the landscapes that we will run
+b <- Sys.time()
+replicate_cycle <- c(1:16)
+clusterApplyLB(cl, x = replicate_cycle, fun = sim_run_cluster, 
+               combo_number = 31, myWorld = myWorld) 
+c <- Sys.time()
+
+
+difftime(b, a)
+# Time to load packages
+
+difftime(c, b)
+# Time to run cluster
+
+stopCluster(cl)
+
