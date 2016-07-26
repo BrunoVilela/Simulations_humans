b634c9c2bf5c31466518c23e5a28e5fe58c4e5fc
BrunoVilela <brunovilelasilva@hotmail.com>
2016-07-26 11:39:58 -0500

    changes in ltt

--- ltt_custom.R
+++ ltt_custom.R
@@ -5,14 +5,18 @@ ltt.custom <- function(x) {
   branch.time2 <- branching.times(x)
   time.ltt2 <- seq(0, max(branch.time2), 1)
   n <- length(time.ltt2)
-  acu.branch2 <- acu.branch.fora2 <- numeric(n)
+  acu.branch2 <- numeric(n)
   for(i in 1:n) {
     which.branch2 <- which(branch.time2 >= time.ltt2[i])
     acu.branch2[i] <-length(which.branch2)
   }
   time <- rank(-time.ltt2) - 1
-  return(cbind(time, acu.branch2))
+  time <- c(time, 0)
+  lineages <- c(acu.branch2, 0) + 1
+  return(cbind(time, lineages))
 }
 
 
-plot(ltt.custom(tree), type = "l") # Tree must be your own tree
+par(mfrow = c(1, 2))
+plot(ltt.custom(tree), type = "l") # Tree must be your own tree from myOut
+ltt(tree, log.lineages = F, gamma = TRUE)
