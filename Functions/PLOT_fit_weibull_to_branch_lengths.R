1a13472d5a10bb59ac651e22fcb1f453e86fde5f
Ty Tuff <tuff@wustl.edu>
2016-08-05 13:29:41 -0500

    branch length plot work

--- Functions/PLOT_fit_weibull_to_branch_lengths.R
+++ Functions/PLOT_fit_weibull_to_branch_lengths.R
@@ -1,32 +1,43 @@
 
+which_model <- sea
+
 ##################################################################################
-weibull_plots <- function(data.result_sub){
-	#data.result_sub <-cbind(a$DF, b$DF, c$DF, d$DF)
-	
+FarmWeibullPlots <- function(which_model1, which_model2, which_model3, which_model4 ){
+		
 	colors <-  c("cornflowerblue", "firebrick", "darkgreen", "orange")
 	
+	data.result_sub_1 <- unlist(which_model1[[9]])
+	data.result_sub_2 <- unlist(which_model2[[9]])
+	data.result_sub_3 <- unlist(which_model3[[9]])
+	data.result_sub_4 <- unlist(which_model4[[9]])
 	
 	
+	blankplot(c(0,5), c(-100, 5000))
 
-	sd <- sd(data.result_sub[,1], na.rm=TRUE)
-	polygon(x=c(0.75,0.75,1.25,1.25), y=c(mean(data.result_sub[,1], na.rm=TRUE)-sd, mean(data.result_sub[,1], na.rm=TRUE)+sd, mean(data.result_sub[,1], na.rm=TRUE)+sd,mean(data.result_sub[,1], na.rm=TRUE)-sd), col=adjustcolor("grey",alpha=.9), border=adjustcolor("grey",alpha=.9))
-	sd <- sd(data.result_sub[,2], na.rm=TRUE)
-	polygon(x=c(1.75,1.75,2.25,2.25), y=c(mean(data.result_sub[,2], na.rm=TRUE)-sd, mean(data.result_sub[,2], na.rm=TRUE)+sd, mean(data.result_sub[,2], na.rm=TRUE)+sd,mean(data.result_sub[,2], na.rm=TRUE)-sd), col=adjustcolor("grey",alpha=.9), border=adjustcolor("grey",alpha=.9))
-	sd <- sd(data.result_sub[,3], na.rm=TRUE)
-	polygon(x=c(2.75,2.75,3.25,3.25), y=c(mean(data.result_sub[,3], na.rm=TRUE)-sd, mean(data.result_sub[,3], na.rm=TRUE)+sd, mean(data.result_sub[,3], na.rm=TRUE)+sd,mean(data.result_sub[,3], na.rm=TRUE)-sd), col=adjustcolor("grey",alpha=.9), border=adjustcolor("grey",alpha=.9))
-	sd <- sd(data.result_sub[,4], na.rm=TRUE)
-	polygon(x=c(3.75,3.75,4.25,4.25), y=c(mean(data.result_sub[,4], na.rm=TRUE)-sd, mean(data.result_sub[,4], na.rm=TRUE)+sd, mean(data.result_sub[,4], na.rm=TRUE)+sd,mean(data.result_sub[,4], na.rm=TRUE)-sd), col=adjustcolor("grey",alpha=.9), border=adjustcolor("grey",alpha=.9))
+	sd <- sd(data.result_sub_1, na.rm=TRUE)
+	polygon(x=c(0.75,0.75,1.25,1.25), y=c(mean(data.result_sub_1, na.rm=TRUE)-sd, mean(data.result_sub_1, na.rm=TRUE)+sd, mean(data.result_sub_1, na.rm=TRUE)+sd,mean(data.result_sub_1, na.rm=TRUE)-sd), col=adjustcolor("grey",alpha=.9), border=adjustcolor("grey",alpha=.9))
 	
-	lines(x=c(0.50,1.50), y=rep(mean(data.result_sub[,1], na.rm=TRUE),2), col=colors[1], lwd=2)
-	lines(x=c(1.50,2.50), y= rep(mean(data.result_sub[,2], na.rm=TRUE),2), col=colors[2], lwd=2)
-	lines(x=c(2.50,3.50), y= rep(mean(data.result_sub[,3], na.rm=TRUE),2), col=colors[3], lwd=2)
-	lines(x=c(3.50,4.50), y= rep(mean(data.result_sub[,4], na.rm=TRUE),2), col=colors[4], lwd=2)
+	sd <- sd(data.result_sub_2, na.rm=TRUE)
+	polygon(x=c(1.75,1.75,2.25,2.25), y=c(mean(data.result_sub_2, na.rm=TRUE)-sd, mean(data.result_sub_2, na.rm=TRUE)+sd, mean(data.result_sub_2, na.rm=TRUE)+sd,mean(data.result_sub_2, na.rm=TRUE)-sd), col=adjustcolor("grey",alpha=.9), border=adjustcolor("grey",alpha=.9))
 	
-	points(jitter(rep(1, length(data.result_sub[,1]))), data.result_sub[,1], col=adjustcolor(colors[1], alpha=.8))
-	points(jitter(rep(2, length(data.result_sub[,2]))), data.result_sub[,2], col=adjustcolor(colors[2], alpha=.8))
-	points(jitter(rep(3, length(data.result_sub[,3]))), data.result_sub[,3], col=adjustcolor(colors[3], alpha=.8))
-	points(jitter(rep(4, length(data.result_sub[,4]))), data.result_sub[,4], col=adjustcolor(colors[4], alpha=.8))
+	sd <- sd(data.result_sub_3, na.rm=TRUE)
+	polygon(x=c(2.75,2.75,3.25,3.25), y=c(mean(data.result_sub_3, na.rm=TRUE)-sd, mean(data.result_sub_3, na.rm=TRUE)+sd, mean(data.result_sub_3, na.rm=TRUE)+sd,mean(data.result_sub_3, na.rm=TRUE)-sd), col=adjustcolor("grey",alpha=.9), border=adjustcolor("grey",alpha=.9))
+	
+	sd <- sd(data.result_sub_4, na.rm=TRUE)
+	polygon(x=c(3.75,3.75,4.25,4.25), y=c(mean(data.result_sub_4, na.rm=TRUE)-sd, mean(data.result_sub_4, na.rm=TRUE)+sd, mean(data.result_sub_4, na.rm=TRUE)+sd,mean(data.result_sub_4, na.rm=TRUE)-sd), col=adjustcolor("grey",alpha=.9), border=adjustcolor("grey",alpha=.9))
+	
+	
+	lines(x=c(0.50,1.50), y=rep(mean(data.result_sub_1, na.rm=TRUE),2), col=colors[1], lwd=2)
+	lines(x=c(1.50,2.50), y= rep(mean(data.result_sub_2, na.rm=TRUE),2), col=colors[2], lwd=2)
+	lines(x=c(2.50,3.50), y= rep(mean(data.result_sub_3, na.rm=TRUE),2), col=colors[3], lwd=2)
+	lines(x=c(3.50,4.50), y= rep(mean(data.result_sub_4, na.rm=TRUE),2), col=colors[4], lwd=2)
+	
+	points(jitter(rep(1, length(data.result_sub_1)),7), data.result_sub_1, col=adjustcolor(colors[1], alpha=.5))
+	points(jitter(rep(2, length(data.result_sub_2)),5), data.result_sub_2, col=adjustcolor(colors[2], alpha=.5))
+	points(jitter(rep(3, length(data.result_sub_3)),4), data.result_sub_3, col=adjustcolor(colors[3], alpha=.5))
+	points(jitter(rep(4, length(data.result_sub_4)),2), data.result_sub_4, col=adjustcolor(colors[4], alpha=.5))
 	
 }
 
-
+FarmWeibullPlots(sea, sea.D, sea.T, sea.DT)
+max(data.result_sub)
