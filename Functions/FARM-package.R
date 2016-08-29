d7a2815e36e65c1cf0d3fa3db7b51691e0c90fb0
BrunoVilela <brunovilelasilva@hotmail.com>
2016-08-29 11:37:37 -0500

    New functions from the package

---
+++ Functions/FARM-package.R
@@ -0,0 +1,17 @@
+#' @importFrom Rcpp evalCpp
+#' @importFrom phytools ltt
+#' @importFrom diversitree make.musse
+#' @importFrom diversitree find.mle
+#' @importFrom diversitree starting.point.musse
+#' @importFrom caper comparative.data
+#' @importFrom caper phylo.d
+#' @importFrom stats cophenetic na.omit runif setNames var
+#' @importFrom utils head
+#' @import picante
+#' @import ape
+#' @export NewTip DropTip TheOriginOfSpecies makePhy Arisal parameters getTargets
+#' @export extinct BuildWorld Diffusion Extinction RunSim speciate Speciation SpeciationTakeOver
+#' @export TakeOver sub.TakeOver RunSimUltimate RunSim2 RunSimUltimate2 combo_of_choice evol.distinct2
+#' @export DivDep Dsig bd Module_2
+#' @useDynLib FARM
+NULL
